356bcbd23c3288a8d36fde5aa4511cee3f0dc266
satyajeet sahoo <116475734+satyajeetsahoo2001@users.noreply.github.com>
2025-01-30 14:45:54 +0530

    Delete studentData/src directory

--- studentData/src/main/java/com/project/student/repository/CollegeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.student.repository;
-
-import com.project.student.College.clg.Clg;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CollegeRepository extends JpaRepository<Clg,Integer> {
-}
--- studentData/src/main/java/com/project/student/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.student.repository;
-
-import com.project.student.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student,Integer> {
-
-}
